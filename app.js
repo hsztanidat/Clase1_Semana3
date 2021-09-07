08985871beebf30761d49e251facab2c2fb69a16
Ian Nawrocki <MrRocotto@MacBook-Air-de-Andrea.local>
2021-09-06 22:59:11 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+for(let i = 0; i<= 20; i++) {
+    if (i%2 === 0) {
+      console.log(`${i} es par`)
+    }
+    else {
+      console.log(`${i} es impar`)
+    }
+  }
+  console.log("\n\n")
+  
+  for(let i=0; i<=10; i++) {
+    console.log(`${i} * 9 = ${i*9}`)
+  }
+  console.log("\n\n")
+  
+  for(let num1 = 1; num1<=10; num1++){
+    for(let num2 = 1; num2<=10; num2++) {
+      console.log(`${num1} * ${num2} = ${num1 * num2}`)
+    }
+    console.log("\n")
+  }
